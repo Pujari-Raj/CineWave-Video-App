5f2c9ce17ef12b1a1bfcf9efdbce9591eca4e00d
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-11-08 15:43:58 +0530

    added fiterdata and libraryslice

---
+++ src/utilities/librarySlice.js
@@ -0,0 +1,30 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const librarySlice = createSlice({
+  name: "library",
+  initialState: {
+    watchlater: localStorage.getItem("watchlaterdata")
+      ? JSON.parse(localStorage.getItem("watchlaterdata"))
+      : [],
+  },
+  reducers: {
+    addVideo : (state, action) => {
+        state.watchlater.push(action.payload);
+        localStorage.setItem("watchlaterdata", JSON.stringify(state.watchlater));
+    },
+    removeVideo: (state, action) => {
+        state.watchlater = state.watchlater.filter(
+            (item) => item!= action.payload,
+        );
+        localStorage.getItem("watchlaterdata", JSON.stringify(state.watchlater));
+    },
+    clearVideos:(state, action) => {
+        state.watchlater = [];
+        localStorage.removeItem("watchlaterdata")
+    }
+  },
+});
+
+export const {addVideo, removeVideo, clearVideos} = librarySlice.actions;
+
+export default librarySlice.reducer;
