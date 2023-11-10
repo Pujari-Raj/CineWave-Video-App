151ebba12da5f9dc9bbe5bd27e4165889644da45
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-11-10 13:59:48 +0530

    added commenstsection and commentcard component

---
+++ src/components/CommentCard.jsx
@@ -0,0 +1,121 @@
+import React, { useState } from "react";
+import { calculateViews, calculateTime } from "../utilities/useMathHelpers";
+import { LikeSVG, DislikeSVG } from "../utilities/SVG";
+
+const ReadMore = ({ children, isReadMore, setIsReadMore }) => {
+  const text = children;
+  return (
+    <pre className="videoFullDesc">
+      {isReadMore ? text?.slice(0, 200) + "..." : text}
+      {text?.length > 200 && (
+        <div
+          onClick={() => setIsReadMore(!isReadMore)}
+          className="commentHideBtn"
+        >
+          {isReadMore ? "Read more" : "Show less"}
+        </div>
+      )}
+    </pre>
+  );
+};
+
+const CommentCard = ({ videoTitle, data }) => {
+  const [isHiddenReply, setIsHiddenReply] = useState(false);
+  const [isReadMore, setIsReadMore] = useState(false);
+  console.log("comment-card->"+data);
+
+  return (
+    <>
+      <div className="commentBox">
+        <div className="commentDefIcon">
+          {
+            <img
+              src={
+                data?.snippet?.topLevelComment?.snippet
+                  ?.authorProfileImageUrl ||
+                data?.snippet?.authorProfileImageUrl
+              }
+              alt=""
+            />
+          }
+        </div>
+        <div className="">
+          <div style={{ display: "flex" }}>
+            <span
+              className={
+                videoTitle ==
+                (data?.snippet?.topLevelComment?.snippet?.authorDisplayName ||
+                  data?.snippet?.authorDisplayName)
+                  ? "commentAuthor commentTitle"
+                  : "commentTitle"
+              }
+            >
+              {"@"}
+              {data?.snippet?.topLevelComment?.snippet?.authorDisplayName ||
+                data?.snippet?.authorDisplayName}
+            </span>
+            <span className="commentTime">
+              {calculateTime(
+                data?.snippet?.topLevelComment?.snippet?.publishedAt ||
+                  data?.snippet?.publishedAt
+              )}
+            </span>
+          </div>
+          <div style={{ marginTop: "5px" }}>
+            <ReadMore
+              isReadMore={isReadMore}
+              setIsReadMore={setIsReadMore}
+              children={
+                data?.snippet?.topLevelComment?.snippet?.textDisplay ||
+                data?.snippet?.textDisplay
+              }
+            />
+          </div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="commentLikes">
+              <LikeSVG />
+              {calculateViews(
+                data?.snippet?.topLevelComment?.snippet?.likeCount ||
+                  data?.snippet?.likeCount
+              )}
+              <DislikeSVG />
+            </div>
+            <div className="replyBtn">Reply</div>
+          </div>
+          {data?.snippet?.totalReplyCount > 0 && (
+            <div
+              className="repliesBtn"
+              onClick={() => {
+                setIsHiddenReply(!isHiddenReply);
+              }}
+            >
+              <svg height="24" width="24" viewBox="0 0 24 24">
+                {isHiddenReply ? (
+                  <path d="M18.4 14.6 12 8.3l-6.4 6.3.8.8L12 9.7l5.6 5.7z"></path>
+                ) : (
+                  <path d="m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z"></path>
+                )}
+              </svg>
+              {data?.snippet?.totalReplyCount > 1
+                ? data?.snippet?.totalReplyCount + " replies"
+                : data?.snippet?.totalReplyCount + " reply"}
+            </div>
+          )}
+          {data?.replies &&
+            isHiddenReply &&
+            data?.replies?.comments.map((commentdata) => {
+              return (
+                <CommentCard
+                  videoTitle={videoTitle}
+                  data={commentdata}
+                  key={commentdata?.id}
+                />
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CommentCard;
---
+++ src/components/CommentSection.jsx
@@ -0,0 +1,143 @@
+import React, { useState, useEffect } from "react";
+import { formatNumber } from "../utilities/useMathHelpers";
+import CommentCard from "../components/CommentCard";
+import useFetch from "../utilities/useFetch";
+import Spinner from "../components/Spinner";
+import { SortSVG } from "../utilities/SVG";
+
+const CommentSection = ({ videoTitle, videoID, comments }) => {
+  const [commentData, setCommentData] = useState([]);
+  const [nextPageToken, setNextPageToken] = useState("");
+  const [commentState, setCommentState] = useState(true);
+  const [isCommentLoading, setIsCommentLoading] = useState(false);
+  const [commentType, setCommentType] = useState("relevance");
+  const [commentBox, setCommentBox] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 899px)");
+    if (mediaQuery.matches) setCommentState(false);
+  }, []);
+
+  useEffect(() => {
+    useFetch(
+      `commentThreads?part=snippet%2Creplies&maxResults=25&order=${commentType}&textFormat=plainText&videoId=${videoID}`
+    ).then((data) => {
+      setCommentData(data?.items);
+      setNextPageToken(data?.nextPageToken);
+      // setIsCommentLoading(false);
+    });
+  }, [videoID, commentType]);
+
+  const loadMoreComments = () => {
+    useFetch(
+      `commentThreads?part=snippet%2Creplies&maxResults=25&order=${commentType}&textFormat=plainText&videoId=${videoID}&pageToken=${nextPageToken}`
+    ).then((data) => {
+      setCommentData((prev) => [...prev, ...data?.items]);
+      setNextPageToken(data?.nextPageToken);
+      setIsCommentLoading(false);
+    });
+  };
+
+  return (
+    <>
+      <div
+        className="commentToggle"
+        onClick={() => setCommentState(!commentState)}
+      >
+        comments
+        <span>
+          <svg height="24" width="24" viewBox="0 0 24 24" className="sortBtn">
+            {commentState ? (
+              <path d="M18.4 14.6 12 8.3l-6.4 6.3.8.8L12 9.7l5.6 5.7z"></path>
+            ) : (
+              <path d="m18 9.28-6.35 6.35-6.37-6.35.72-.71 5.64 5.65 5.65-5.65z"></path>
+            )}
+          </svg>
+        </span>
+      </div>
+      {commentState && (
+        <div className="videoCmntSection">
+          <div className="commentDetails">
+            {formatNumber(comments)}
+            {commentData && (
+              <span
+                className="sortBtn"
+                onClick={() => setCommentBox(!commentBox)}
+              >
+                <SortSVG />
+                {" Sort by"}
+                {commentBox && (
+                  <div className="commentSortBox">
+                    <div
+                      className={
+                        commentType == "relevance"
+                          ? "activeCommentItem commentSortBoxItems"
+                          : "commentSortBoxItems"
+                      }
+                      onClick={() => {
+                        setCommentType("relevance");
+                        setCommentBox(false);
+                      }}
+                    >
+                      Top comments
+                    </div>
+                    <div
+                      className={
+                        commentType == "time"
+                          ? "activeCommentItem commentSortBoxItems"
+                          : "commentSortBoxItems"
+                      }
+                      onClick={() => {
+                        setCommentType("time");
+                        setCommentBox(false);
+                      }}
+                    >
+                      Newest first
+                    </div>
+                  </div>
+                )}
+              </span>
+            )}
+          </div>
+          <div className="commentsContainer">
+            {commentData ? (
+              commentData.length > 0 ? (
+                commentData?.map((commentData) => {
+                  return (
+                    <CommentCard
+                      videoTitle={videoTitle}
+                      data={commentData}
+                      key={commentData?.id}
+                    />
+                  );
+                })
+              ) : (
+                <div className="centerDiv">No comment on this video</div>
+              )
+            ) : (
+              <div className="centerDiv">Comments are turned off</div>
+            )}
+            {commentData && nextPageToken && !isCommentLoading ? (
+              <div className="loadBtnContainer">
+                <div
+                  onClick={() => {
+                    setIsCommentLoading(true);
+                    loadMoreComments();
+                  }}
+                  className="loadMoreBtn"
+                >
+                  Load more comments
+                </div>
+              </div>
+            ) : (
+              <></>
+            )}
+            {isCommentLoading && <Spinner />}
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default CommentSection;
